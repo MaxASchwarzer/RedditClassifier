788b8ab6d044c8405c6812facdbe9f22e9f9952d
MaxSchwarzer <maxon999@comcast.net>
2016-10-03 20:27:19 -0700

    fixed tab/space indentation

--- build_dictionary.py
+++ build_dictionary.py
@@ -7,34 +7,34 @@ import fileinput
 from collections import OrderedDict
 
 def main():
-    for filename in sys.argv[1:]:
-        print 'Processing', filename
-        word_freqs = OrderedDict()
-        with open(filename, 'r') as f:
-            for line in f:
+	for filename in sys.argv[1:]:
+		print 'Processing', filename
+		word_freqs = OrderedDict()
+		with open(filename, 'r') as f:
+			for line in f:
 				line = line.split("\t")[0]
-                words_in = line.strip().split(' ')
-                for w in words_in:
-                    if w not in word_freqs:
-                        word_freqs[w] = 0
-                    word_freqs[w] += 1
-        words = word_freqs.keys()
-        freqs = word_freqs.values()
+				words_in = line.strip().split(' ')
+				for w in words_in:
+					if w not in word_freqs:
+						word_freqs[w] = 0
+					word_freqs[w] += 1
+		words = word_freqs.keys()
+		freqs = word_freqs.values()
 
-        sorted_idx = numpy.argsort(freqs)
-        sorted_words = [words[ii] for ii in sorted_idx[::-1]]
+		sorted_idx = numpy.argsort(freqs)
+		sorted_words = [words[ii] for ii in sorted_idx[::-1]]
 
-        worddict = OrderedDict()
-        worddict['eos'] = 0
-        worddict['UNK'] = 1
-        for ii, ww in enumerate(sorted_words):
-            worddict[ww] = ii+2
+		worddict = OrderedDict()
+		worddict['eos'] = 0
+		worddict['UNK'] = 1
+		for ii, ww in enumerate(sorted_words):
+			worddict[ww] = ii+2
 
-        with open('%s.pkl'%filename, 'wb') as f:
-            pkl.dump(worddict, f)
+		with open('%s.pkl'%filename, 'wb') as f:
+			pkl.dump(worddict, f)
 
-        print 'Done'
+		print 'Done'
 
 
 if __name__ == '__main__':
-    main()
+	main()
