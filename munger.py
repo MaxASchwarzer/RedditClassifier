5d9bb64455603b41d345cf94bc309c4259c4b8ee
MaxSchwarzer <maxon999@comcast.net>
2016-10-03 21:22:29 -0700

    Fixed indexing for python 3

--- munger.py
+++ munger.py
@@ -3,7 +3,7 @@ import numpy as np
 from nltk.tokenize import WordPunctTokenizer
 
 
-df1 = pd.read_csv("./comments.tsv", "\t", header = None, names = ["uid1", "subreddit", "uid2", "postid", "parentid", "threadid", "text"])
+df1 = pd.read_csv("./comments_partial.tsv", "\t", header = None, names = ["uid1", "subreddit", "uid2", "postid", "parentid", "threadid", "text"])
 df2 = pd.read_csv("./actions.tsv", "\t", header = None, names = ["action", "timestamp", "postid", "subreddit", "is_automod"])
 
 del df1 ["uid1"]
@@ -25,9 +25,9 @@ tokenizer = WordPunctTokenizer()
 lowercase = lambda x: (" ").join(tokenizer.tokenize(x.lower()))
 df3 = df3.applymap(lowercase)
 
-testing_df = df3.iloc[:(len(df3)/10)]
-valid_df = df3.iloc[(len(df3)/10):(3*len(df3)/20)]
-training_df = df3.iloc[(3*len(df3)/20):]
+testing_df = df3.iloc[:int(len(df3)/10)]
+valid_df = df3.iloc[int((len(df3)/10)):int((3*len(df3)/20))]
+training_df = df3.iloc[int((3*len(df3)/20)):]
 
 testing_df.to_csv("reddit_comment_testing.tsv", "\t", header = False, index = False)
 valid_df.to_csv("reddit_comment_valid.tsv", "\t", header = False, index = False)
